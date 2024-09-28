03e8eeb9f38100f1e17faa1a1c636ec3ce1b2591
Prathamesh <prathameshcode58@gmail.com>
2024-09-29 02:25:19 +0530

    otp apis

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { sendOtp, verifyOtp } = require("../Controllers/otpController");
 const {
   registerUser,
   loginUser,
@@ -11,5 +12,18 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
 router.get("/",getUsers)
+router.post("/send-otp", async (req, res) => {
+  const email = "prathameshpalve08@gmail.com";
+  // const { email } = req.body.emailId;
+  const result = await sendOtp(email);
+  res.json(result);
+});
+
+// Route to verify OTP
+router.post("/verify-otp", (req, res) => {
+  const { email, otp } = req.body;
+  const result = verifyOtp(email, otp);
+  res.json(result);
+});
 
 module.exports = router;
